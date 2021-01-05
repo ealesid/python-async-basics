302bfc35683362366f9f16dee95ac487d2a3c34e
ealesid <ealesid@gmail.com>
2021-01-05 08:55:30 +0300

    7- download files synchronously

---
+++ 7_async.py
@@ -0,0 +1,32 @@
+import requests
+from time import time
+
+
+URL = 'https://loremflickr.com/320/240'
+
+
+def get_file(url):
+    r = requests.get(url, allow_redirects=True)
+    return r
+
+
+def write_file(response):
+    filename = response.url.split('/')[-1]
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
+def main():
+    t0 = time()
+
+    for i in range(10):
+        write_file(get_file(URL))
+    
+    print('time >\tt', time() - t0)
+
+
+if __name__ == "__main__":
+    main()
+
+#################################################
